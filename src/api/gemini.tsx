17f54346b156f6a59645b968fd23586d3aafffe4
Zade Scoxp <78777405+zadescoxp@users.noreply.github.com>
2024-12-05 01:02:55 +0530

    fix

--- src/api/gemini.tsx
+++ src/api/gemini.tsx
@@ -12,27 +12,5 @@ const model = genai.getGenerativeModel({
     Language: Whatever the users feels good with you will get to know what language they are comfortable with
         `,
 });
-// const chat = model.startChat({
-//     history: [
-//       {
-//         role: "user",
-//         parts: [{ text: "Hello" }],
-//       },
-//       {
-//         role: "model",
-//         parts: [{ text: "Great to meet you. What would you like to know?" }],
-//       },
-//     ],
-//   });
-//   let result = await chat.sendMessageStream("I have 2 dogs in my house.");
-//   for await (const chunk of result.stream) {
-//     const chunkText = chunk.text();
-//     process.stdout.write(chunkText);
-//   }
-//   result = await chat.sendMessageStream("How many paws are in my house?");
-//   for await (const chunk of result.stream) {
-//     const chunkText = chunk.text();
-//     process.stdout.write(chunkText);
-//   }
 
 export { model };
